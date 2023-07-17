5ecca75243d9b8720b6c35191403e25f2bbdfde3
Dmitry-kl <i6169696@gmail.com>
2023-07-17 23:46:23 +0300

    admin add first menu

--- src/Components/Admin/Admin.tsx
+++ src/Components/Admin/Admin.tsx
@@ -1,5 +1,75 @@
 import React, { FC } from 'react';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import AppBar from '@mui/material/AppBar';
+import CssBaseline from '@mui/material/CssBaseline';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import Typography from '@mui/material/Typography';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import { Card, CardActionArea, CardContent, CardMedia, Grid } from '@mui/material';
+
+const drawerWidth = 300;
 
 export const Admin: FC = () => {
-  return <div>This is admin</div>;
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
+        <Toolbar>
+          <Typography variant="h6" noWrap component="div">
+            Админ панель
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        variant="permanent"
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          ['& .MuiDrawer-paper']: { width: drawerWidth, boxSizing: 'border-box' },
+        }}
+      >
+        <Toolbar />
+        <Box sx={{ overflow: 'auto' }}>
+          <List>
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <InboxIcon />
+                </ListItemIcon>
+                <ListItemText primary={'Партнерские статьи'} />
+              </ListItemButton>
+            </ListItem>
+          </List>
+        </Box>
+      </Drawer>
+      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+        <Toolbar />
+        <Grid container spacing={4}>
+          {[1, 2, 3, 4, 5, 6].map((el, index) => (
+            <Grid item xs={3} key={index}>
+              <Card>
+                <CardActionArea>
+                  <CardMedia component="img" height="140" image="/static/images/cards/contemplative-reptile.jpg" alt="green iguana" />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      Lizard
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
+    </Box>
+  );
 };
--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import React, { ReactHTMLElement } from 'react';
+import React from 'react';
 import { categoryNames } from '../../utils';
 import logo from '../../images/logo.svg';
 import { Link } from 'react-router-dom';
@@ -12,14 +12,14 @@ export const Header: React.FC<IProps> = ({ category }) => {
     <header className="header">
       <div className="container grid">
         <nav className="navigation">
-          <Link to={'/'} className="navigation__logo">
+          <Link to={'./'} className="navigation__logo">
             <img src={logo} alt="logo" />
           </Link>
           <ul className="navigation__list">
             {['index', 'fashion', 'technologies', 'sport', 'other'].map((el) => {
               return (
                 <li className="navigation__item" key={el}>
-                  <Link to={el} className={`navigation__link ${category === el ? 'navigation__link--active' : ''}`}>
+                  <Link to={`/${el}`} className={`navigation__link ${category === el ? 'navigation__link--active' : ''}`}>
                     {/*@ts-ignore*/}
                     {categoryNames[el]}
                   </Link>
